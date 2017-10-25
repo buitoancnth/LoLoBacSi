441868ec3d2d103793bc193529bbac49ebd2018a
lesan1995 <lesan1995@gmail.com>
2017-10-25 14:32:44 +0700

    Init Project

---
+++ app/src/main/java/alobacsi/alobacsi/DieuKhoanActivity.java
@@ -0,0 +1,13 @@
+package alobacsi.alobacsi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DieuKhoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dieu_khoan);
+    }
+}
