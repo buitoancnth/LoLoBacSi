410eea825bd9a22f0611351267f40726e5cd0b97
lesan1995 <lesan1995@gmail.com>
2017-11-26 09:41:50 +0700

    login activity

---
+++ app/src/main/java/alobacsi/alobacsi/BenhNhan/RegisterActivity.java
@@ -0,0 +1,21 @@
+package alobacsi.alobacsi.BenhNhan;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+import alobacsi.alobacsi.R;
+
+/**
+ * Created by lequa on 26/11/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
